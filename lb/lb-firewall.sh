5d07891e70982408070a9ce1084e69d3230193bb
Eric Quagrainie <e.quagrainie@alustudent.com>
2026-07-15 17:00:00 +0200

    Add default DROP policy to firewall scripts

--- lb/lb-firewall.sh
+++ lb/lb-firewall.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
+iptables -P INPUT DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
--- web/web-firewall.sh
+++ web/web-firewall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
+iptables -P INPUT DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
 iptables -A INPUT -p tcp -s 172.25.0.10 --dport 80 -j ACCEPT
-
