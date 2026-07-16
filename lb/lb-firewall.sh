bd61ce5366a72542433316b8d937ac4fb00c9041
Eric Quagrainie <e.quagrainie@alustudent.com>
2026-07-16 17:00:00 +0200

    Add iptables firewall scripts for web and lb

---
+++ lb/lb-firewall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
---
+++ web/web-firewall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+iptables -A INPUT -p tcp -s 172.25.0.10 --dport 80 -j ACCEPT
+
